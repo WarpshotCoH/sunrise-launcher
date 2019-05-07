1d2748d9000941f4c7f49c3a1eafaf0d3a8e5a1c
Augustus Mayo <gusmayo@gmail.com>
2019-05-07 16:30:04 -0500

    Revert download thread changes

--- downloadui.py
+++ downloadui.py
@@ -6,7 +6,6 @@ from downloader import Downloader, FileDownload, DownloaderState
 
 class DownloadUI(QObject):
     launch = Signal()
-    trigger = Signal()
 
     def __init__(self, progressBar, fileBar, label, button, parent=None):
         super(DownloadUI, self).__init__(parent)
@@ -15,13 +14,10 @@ class DownloadUI(QObject):
         self.fileBar = fileBar
         self.label = label
         self.button = button
-        self.thread = QThread()
+        self.downloadThread = None
         self.downloader = None
 
         self.button.clicked.connect(self.startDownload)
-
-        # Boot the background thread
-        self.thread.start()
 
     def hide(self):
         self.progressBar.hide()
@@ -46,27 +42,32 @@ class DownloadUI(QObject):
         self.launch.emit()
 
     def verifyDownload(self):
-        # Before verifying, make sure existing downloaders have been cleaned up
-        if self.downloader:
-            self.downloader.pause()
-
+        self.shutdown()
         self.downloader = Downloader(self.containers, self.installPath, True)
         self.runInBackground(self.downloader.verify)
 
     def startDownload(self):
-        # Before downloading, make sure existing downloaders have been cleaned up
-        if self.downloader:
-            self.downloader.pause()
-
+        # Before verifying or downloading, make sure existing downloaders and
+        # threads have been cleaned up
+        self.shutdown()
         self.downloader = Downloader(self.containers, self.installPath)
         self.runInBackground(self.downloader.download)
 
+    # TODO: Startup on this feels slow (due to thread spawn maybe?). Can we
+    #       reuse the thread / downloader and instead use a custom slot + signal
+    #       for triggering instead of thread start?
     def runInBackground(self, fn):
-        print("Start background task", fn)
+        print("Start background download")
 
         # We do not want multiple downloads to start while waiting for the thread
         # to initialize
         self.disableButton()
+
+        # Init the download background thread
+        # TODO: Can not seem to figure out how to spawn a single QThread and then
+        #       continually reused by placing new work on to it. Seems like
+        #       something that we should be able to do.
+        self.downloadThread = QThread()
 
         # Connect up to the files progress events
         self.downloader.start.connect(self.onStart)
@@ -79,29 +80,19 @@ class DownloadUI(QObject):
         self.downloader.fileVerify.connect(self.onFileVerify)
 
         # Move the download manager to the background thread
-        self.downloader.moveToThread(self.thread)
+        self.downloader.moveToThread(self.downloadThread)
 
-        # Schedule the downloader to start once the thread is active
         # Once the thread is ready, we can re-enable the button. We attach the
         # enable listener first because it has to run prior to the download
         # starting. Once the download starts the thread will be busy until it
         # completes / errors / or is cancelled
-        if self.thread.isRunning():
-            print("Download thread already active. Starting", fn)
+        self.downloadThread.started.connect(self.enableButton)
 
-            # TODO: There should be a better way to write this
-            try:
-                self.trigger.disconnect()
-            except Exception:
-                pass
+        # Schedule the downloader to start once the thread is active
+        self.downloadThread.started.connect(fn)
 
-            self.trigger.connect(fn)
-            self.trigger.emit()
-            self.enableButton()
-        else:
-            print("Download thread not active. Scheduling", fn)
-            self.thread.started.connect(fn)
-            self.thread.started.connect(self.enableButton)
+        # Boot the background thread
+        self.downloadThread.start()
 
     def enableButton(self):
         self.button.setEnabled(True)
@@ -116,9 +107,9 @@ class DownloadUI(QObject):
         if self.downloader:
             self.downloader.pause()
 
-        if self.thread:
-            self.thread.quit()
-            self.thread.wait()
+        if self.downloadThread:
+            self.downloadThread.quit()
+            self.downloadThread.wait()
 
     @Slot(str, int, int, int)
     def onStart(self, name, pMin, pStart, pMax):
@@ -160,6 +151,19 @@ class DownloadUI(QObject):
         self.button.clicked.disconnect()
         self.button.clicked.connect(buttonAction[state])
 
+        # TODO: If QThread can be re-used over and over then this is not necessary.
+        #       In the meantime we tear down the thread whenever progress stops and
+        #       create a new one progress begins again
+        if state == DownloaderState.PAUSED or state == DownloaderState.COMPLETE or state == DownloaderState.DOWNLOAD_FAILED or state == DownloaderState.VERIFICATION_FAILED:
+
+            # If we had a running thread that needs to be stopped, ensure that it
+            # is stopped before we allow a new download to be started with a new
+            # download thread
+            self.downloadThread.finished.connect(self.enableButton)
+            self.downloadThread.quit()
+        else:
+            self.enableButton()
+
         if state == DownloaderState.DOWNLOAD_FAILED and filename:
             self.label.setText("Failed to download " + filename)
 
@@ -169,8 +173,6 @@ class DownloadUI(QObject):
         # On a pause request or a complete we clear out any progress text
         if state == DownloaderState.PAUSED or state == DownloaderState.COMPLETE:
             self.label.setText("")
-
-        self.enableButton()
 
     @Slot(int, int, int, str)
     def onFileStart(self, pMin, pStart, pMax, filename):
