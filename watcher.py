64e5c5b123c1dd5c80a0189671d781dde4c20d3a
Augustus Mayo <gusmayo@gmail.com>
2019-05-21 23:03:29 -0500

    Work on fixing watcher timers running on main thread

--- watcher.py
+++ watcher.py
@@ -9,21 +9,25 @@ import requests
 class WatcherPool(QObject):
     updated = Signal(str, str)
     startTrigger = Signal()
-    stopTrigger = Signal()
+    stopTrigger = Signal(str)
+    shutdownTrigger = Signal()
 
     def __init__(self, parent=None):
         super(WatcherPool, self).__init__(parent)
         self.watchers = {}
+        self.graveyard = []
         self.thread = QThread()
         self.thread.setObjectName("Watcher")
+        self.thread.start()
 
     def add(self, url):
         if self.watchers.get(url):
-            self.watchers[url].shutdown()
+            self.remove(url)
 
         self.watchers[url] = Watcher(url, self.updated)
         self.watchers[url].moveToThread(self.thread)
-        self.stopTrigger.connect(self.watchers[url].shutdown)
+        self.stopTrigger.connect(self.watchers[url].stop)
+        self.shutdownTrigger.connect(self.watchers[url].shutdown)
 
         if self.thread.isRunning():
             print("Watch thread already running. Starting", url)
@@ -38,15 +42,25 @@ class WatcherPool(QObject):
         else:
             print("Watch thread not running. Scheduling", url)
             self.thread.started.connect(self.watchers[url].start)
-            self.thread.start()
 
     def remove(self, url):
         if self.watchers.get(url):
-            self.watchers[url].shutdown()
-            self.watchers.pop(url, None)
+            self.stopTrigger.emit(url)
 
+            # Append to graveyard so that the timer can gracefully stop
+            # instead of the reference being dropped
+            self.graveyard.append(self.watchers.pop(url))
+
+    @Slot()
     def shutdown(self):
-        self.stopTrigger.emit()
+        self.shutdownTrigger.emit()
+
+        # TODO: Hack to get the pool's thread to run the shutdown requests
+        #       prior to shutting down the thread itself. Stopping the thread
+        #       immediately results in timers in the pool thread being stopped
+        #       by the main thread. This seems problematic? I do not know
+        #       enough of the systems to make a proper call
+        time.sleep(0.25)
 
         self.thread.quit()
         self.thread.wait()
@@ -66,17 +80,26 @@ class Watcher(QObject):
         print("Start watcher", self.url)
         print("Current thread during start", QThread.currentThread().objectName())
         self.timer = QTimer()
-        self.timer.setInterval(60000)
+        self.timer.setInterval(6000)
         self.timer.timeout.connect(self.run)
         self.timer.start()
         self.run()
 
-    @Slot()
-    def shutdown(self):
-        # TODO: Fix QObject::killTimer: Timers cannot be stopped from another thread on shutdown
-        print("Current thread during shutdown", QThread.currentThread().objectName())
+    @Slot(str)
+    def stop(self, url):
+        print("Current thread during watcher stop", QThread.currentThread().objectName(), url)
 
         if self.timer:
+            if url == self.url:
+                print("Stop timer")
+                self.timer.stop()
+
+    @Slot()
+    def shutdown(self):
+        print("Current thread during watcher shutdown", QThread.currentThread().objectName())
+
+        if self.timer:
+            print("Stop timer")
             self.timer.stop()
 
     @Slot()
